eab9e41a35b17a42584eb91df215aacd631ed177
Scott Lowry <Scotth@cpp.edu>
2016-02-01 17:38:06 -0800

    Added material design items to the activity_main_drawer.xml and changed the labels on the items. The communicate group was not changed so i am able to go back and see how it was created incase i want to put something else in there. Changed the default imageview and two textview for the nav_header_main.xml. These are place holders to be populated with their picture, user name and email address, when they log in.

--- app/src/main/java/cpp/scottl/com/handleit/MainActivity.java
+++ app/src/main/java/cpp/scottl/com/handleit/MainActivity.java
@@ -101,15 +101,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_notifications) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_upload) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_pre_post) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_messages) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_account) {
 
         } else if (id == R.id.nav_send) {
 
