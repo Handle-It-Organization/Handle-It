65c1bd9b9accba2586ac6502f161a7d73d70b004
Scott Lowry <Scotth@cpp.edu>
2016-02-13 22:42:15 -0800

    Added Amazon AWS S3 and Contigo. Tried to work with this for adding and storing photos but couldnt get it to work right. there is now a bunch of commented out code that doesnt work that i tried in StartActivity when you click the FAB and get the photo from camera. Also from the QuestionViewerActivity to try and get the Bitmap that was passed into the activity to setImage but wouldnt work

--- app/src/main/java/cpp/scottl/com/handleit/QuestionViewerActivity.java
+++ app/src/main/java/cpp/scottl/com/handleit/QuestionViewerActivity.java
@@ -1,7 +1,14 @@
 package cpp.scottl.com.handleit;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class QuestionViewerActivity extends AppCompatActivity {
 
@@ -9,5 +16,18 @@ public class QuestionViewerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_viewer);
+////////////////////////////////////////////////////////////////////////////////THIS IS WHERE IT BROKE
+        Intent myIntent = getIntent();
+        Bitmap myBitmap = (Bitmap) myIntent.getParcelableExtra("BitmapImage");
+
+        BitmapDrawable ob = new BitmapDrawable(getResources(), myBitmap);
+       // byte[] imageAsBytes = Base64.decode(bmString, Base64.DEFAULT);
+        Log.i("Tag", "After setting the image1");
+      //  Bitmap bmp = BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length);
+
+        ImageView imageView = (ImageView) findViewById(R.id.image_question_viewer);
+     //   imageView.setImageURI();
+       // imageView.setBackgroundDrawable(ob);
+       // imageView.setImageBitmap(myBitmap);
     }
 }
